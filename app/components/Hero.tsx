118f8c67c1bb98f2667ca031b0ea2264d45a978f
Diogo2584 <penafieldp8@gmail.com>
2026-04-14 23:02:50 +0000

    fixed netlify?

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,7 @@ export default function Hero() {
     return (
         <div className="hero" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px', backgroundColor: '#FDF0D5' }}>
             <div className="hero-content" style={{ textAlign: 'center', marginBottom: '20px' }}>
-                <h1 className="heading1">Hi, I'm Diogo Penafiel</h1>
+                <h1 className="heading1 text-black-500">Hi, I'm Diogo Penafiel</h1>
                 <p className="paragraph">A passionate Frontend Developer with expertise in React and Next.js. I create engaging and user-friendly web applications that bring ideas to life.</p>
             </div>
             <div className="hero-image" style={{ display: 'flex', justifyContent: 'center' }}>
